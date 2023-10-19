fff9c9adefc8a3e44451634df9043624a8a0ccee
Kakar13 <116319489+Kakar13@users.noreply.github.com>
2023-10-19 01:51:44 -0400

    First git

---
+++ React-Portfolio-master/src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+    "imagebaseurl":"https://www.linkedin.com/in/vikaskakar2/",
+    "name": "Vikas Kakar",
+    "role": "junior majoring in Electrical and Computer Engineering at The Ohio State University",
+    "linkedinId:": "https://www.linkedin.com/in/vikaskakar2/",
+    "skypeid": "Your skypeid",
+    "roleDescription": " Explore my journey, projects, and passion for innovation in the world of technology.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/vikaskakar2/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Kakar13",//add link
+          "className":"fa fa-github"
+        },
+        {
+          "name":"Twitter",
+          "url":"",//add link
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a junior majoring in Electrical and Computer Engineering at The Ohio State University. Crafting code and forging solutions, I'm the architect of innovation in the digital realm.",
+    "address":"Columbus, OH, United States",
+    // "website":"",
+    "education":[
+      {
+        "UniversityName":"The Ohio State University",
+        "specialization":"Electrical and Computer Engineering w/ specialization in CE",
+        // ".\n",
+        "Graduation":".\nDec",
+        "YearOfPassing":"2025",
+        // "Achievements":"Some Achievements"
+      },
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"eTravelMate Pvt. Ltd.",
+        "specialization":"Software Testing Intern",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2023",
+        "Achievements":"• Successfully ensured the reliability, safety, and user-friendliness of transportation software"
+      },
+      {
+        "CompanyName":"Shakti Agro Industries",
+        "specialization":"Marketing Strategist Intern",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2021",
+        "Achievements":"• Successfully ran an online campaign spreading awareness about a competitive priced agricultural Implements company via various social media platforms and advertising agencies."
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"JAVA"
+      },
+      {
+        "skillname":"C++"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    // "portfolio":[
+    //   {
+    //     "name":"project1",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   },
+    //   {
+    //     "name":"project2",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/project.jpg"
+    //   },
+    //   {
+    //     "name":"project3",
+    //     "description":"mobileapp",  
+    //     "imgurl":"images/portfolio/project2.png"
+    //   },
+    //   {
+    //     "name":"project4",
+    //     "description":"mobileapp",
+    //     "imgurl":"images/portfolio/phone.jpg"
+    //   }
+    // ],
+    // "testimonials":[
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
